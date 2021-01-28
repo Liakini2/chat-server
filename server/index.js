c1ba5405f12f5100c29c7901b98c1c60b3212eaa
Lia Couch <76275077+Liakini2@users.noreply.github.com>
2021-01-28 13:57:45 -0700

    finished

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 const express = require('express')
+const messages_controller = require('./controllers/messages_controller')
 const app = express()
 app.use(express.json())
+app.use(express.static(__dirname + '/../public/build'))
+
+
+app.post('/api/messages', messages_controller.create)
+app.get('/api/messages', messages_controller.read)
+app.put(`/api/messages/:id`, messages_controller.update)
+app.delete(`/api/messages/:id`, messages_controller.delete)
+
+
+
 const port = 3001
-
-
-
-
-
-
-
-
-
-
-
 app.listen(port, ()=>console.log(`Server is running on port ${port}`))
